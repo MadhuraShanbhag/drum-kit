efa4d6783ddc19d39303fa974a1735b6c340e22b
Madhura <shanbhagmadhura99@gmail.com>
2019-02-07 17:07:57 +0530

    Initial commit.

---
+++ index.js
@@ -0,0 +1,53 @@
+// document.querySelector("button").addEventListener("click", handleClick);
+//
+
+var numberOfDrums = document.querySelectorAll(".drum");
+for (var i = 0; i < numberOfDrums.length; i++) {
+  numberOfDrums[i].addEventListener("click", function() {
+    playSound(this.innerHTML);
+  });
+}
+
+function playSound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "s":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "d":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "j":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    case "k":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "l":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+
+    default:
+      console.log(key);
+
+  }
+
+}
+
+
+document.addEventListener("keydown", function(event) {
+  playSound(event.key);
+});
